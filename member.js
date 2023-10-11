d5d660018b1b71d3bf45d4a825276d609c4b3f83
Patrick Joel Lehmann <106749268+Paeddy11@users.noreply.github.com>
2023-10-11 14:47:10 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "PHP", "Python"];
+    var member = {
+        name: "Nguyen Van A",
+        age: 20,
+        skills: skills
+    };
+    return member;
+}
